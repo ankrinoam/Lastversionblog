674af4fffae645d894941f1f9cfdb2c22881565f
ankrinoam <85039545+ankrinoam@users.noreply.github.com>
2024-01-23 12:37:59 +0000

    s,

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import streamlit as st
 from openai import OpenAI
 
 # Initialisez le client OpenAI avec la nouvelle méthode
-openai_client = OpenAI(api_key='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')
+openai_client = OpenAI(api_key=os.environ['Openai'])
 
 scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
          "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
